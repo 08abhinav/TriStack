ddc9a8e4014d8205d53d96b2f7a14cf7ec48afa9
08abhinav <abhinavnegi101@gmail.com>
2026-03-02 10:28:43 +0530

    setup for db connection

---
+++ app/backend/db/dbConnection.js
@@ -0,0 +1,11 @@
+import "dotenv/config"
+import mongoose from "mongoose"
+
+export const connectDB = async()=>{
+  try{
+    await mongoose.connect(process.env.MONGODB_URL)
+    console.log("Database connected successfully")
+  }catch(error){
+    console.log("Database connection error: ", error);
+  }
+}
