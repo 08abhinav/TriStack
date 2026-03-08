73e65224760cbf78e257739b15695d8addc122cc
08abhinav <abhinavnegi101@gmail.com>
2026-03-08 12:13:36 +0530

    update: changed the baseurl with .env BASE_URL

--- app/frontend/src/api/todoAPI.js
+++ app/frontend/src/api/todoAPI.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
+const BASE_URL = import.meta.env.BASE_URL
 const API = axios.create({
-  baseURL: "http://localhost:8500",
+  baseURL: BASE_URL,
 });
 
 export const getTodos = () => API.get("/api/tasks");
