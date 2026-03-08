bc3ec0c969d3d4848127234ff3db8e7ae9eaaed0
08abhinav <abhinavnegi101@gmail.com>
2026-03-08 15:00:04 +0530

    update: changed api routes and env variable name

--- app/frontend/src/api/todoAPI.js
+++ app/frontend/src/api/todoAPI.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.BASE_URL
+const BASE_URL = import.meta.env.VITE_BASE_URL
 const API = axios.create({
   baseURL: BASE_URL,
 });
 
-export const getTodos = () => API.get("/api/tasks");
-export const createTodo = (data) => API.post("/api/tasks", data);
-export const updateTodo = (id, data) => API.put(`/api/tasks/${id}`, data);
-export const deleteTodo = (id) => API.delete(`/api/tasks/${id}`);
+export const getTodos = () => API.get("/tasks");
+export const createTodo = (data) => API.post("/tasks", data);
+export const updateTodo = (id, data) => API.put(`/tasks/${id}`, data);
+export const deleteTodo = (id) => API.delete(`/tasks/${id}`);
